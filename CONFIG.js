359f13a3b7baa1a822add593372165b15c5c9fe8
Volkas <88390471+volkas404@users.noreply.github.com>
2024-03-08 00:18:07 +0700

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Anh có điều muốn nói. </br> Anh yêu em!",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Em có yêu anh không ?",
+  buttonYes: "Có yêu",
+  buttonNo: "Không yêu",
+  titleModar: "Anh cũng yêu em",
+  descriptionModar:
+    "Cảm ơn em đã cho anh một cơ hội để anh có thể ở bên cạnh, che chở và bảo vệ em. </br> anh biết anh không được đẹp trai, cũng chẳng lắm tài nhưng anh có một trái tim chân thành. Em chính là cả thế giới của anh.</br> Love you <3",
+};
